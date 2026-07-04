110d6aaab51361d1910cad5a8632fc03f300c76a
Ankit Pandey <ankitdp7275@gmail.com>
2026-07-04 12:18:22 +0530

    Update Day27.java

--- Day27.java
+++ Day27.java
@@ -76,3 +76,28 @@ class Solution {
         return false;
     }
 }
+
+//Find the Duplicate Number
+
+class Solution {
+
+    public int findDuplicate(int[] nums) {
+
+        int slow = nums[0];
+        int fast = nums[0];
+
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        slow = nums[0];
+
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
+    }
+}
