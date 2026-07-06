a2cbbfa077463fe519884cdd8193458e1cfcaaa8
Ankit Pandey <ankitdp7275@gmail.com>
2026-07-06 13:42:26 +0530

    Update Day27.java

--- Day27.java
+++ Day27.java
@@ -136,3 +136,24 @@ class Solution {
         return ans;
     }
 }
+
+//Remove Element
+
+class Solution {
+
+    public int removeElement(int[] nums, int val) {
+
+        int k = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] != val) {
+
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+}
