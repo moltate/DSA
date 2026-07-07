7995f0706dab82ad9aebbc0e8863c26588619862
Ankit Pandey <ankitdp7275@gmail.com>
2026-07-07 17:18:11 +0530

    Create Day28.java

---
+++ Day28.java
@@ -0,0 +1,37 @@
+//Rotate List 
+
+class Solution {
+
+    public ListNode rotateRight(ListNode head, int k) {
+
+        if (head == null || head.next == null || k == 0)
+            return head;
+
+        int length = 1;
+        ListNode tail = head;
+
+        while (tail.next != null) {
+            tail = tail.next;
+            length++;
+        }
+
+        k %= length;
+
+        if (k == 0)
+            return head;
+
+        tail.next = head;
+
+        ListNode newTail = head;
+
+        for (int i = 1; i < length - k; i++) {
+            newTail = newTail.next;
+        }
+
+        ListNode newHead = newTail.next;
+
+        newTail.next = null;
+
+        return newHead;
+    }
+}
