8bd0cc76ff2184775d2cd9591d21501e2f61583f
Ankit Pandey <ankitdp7275@gmail.com>
2026-07-11 21:50:33 +0530

    Update Day28.java

--- Day28.java
+++ Day28.java
@@ -88,3 +88,24 @@ class Solution {
         return dummy.next;
     }
 }
+
+
+//Find the Difference 
+
+class Solution {
+
+    public char findTheDifference(String s, String t) {
+
+        char ans = 0;
+
+        for (char ch : s.toCharArray()) {
+            ans ^= ch;
+        }
+
+        for (char ch : t.toCharArray()) {
+            ans ^= ch;
+        }
+
+        return ans;
+    }
+}
