def958fa1fb7e48dc1647d769053d85233300b07
Ankit Pandey <ankitdp7275@gmail.com>
2026-07-19 21:29:39 +0530

    Update Day30.java

--- Day30.java
+++ Day30.java
@@ -43,3 +43,32 @@ class Solution {
         return sign * result;
     }
 }
+
+//Find the Index of First Occurrence in the String
+
+
+class Solution {
+
+    public int strStr(String haystack, String needle) {
+
+        int n = haystack.length();
+        int m = needle.length();
+
+        for (int i = 0; i <= n - m; i++) {
+
+            int j = 0;
+
+            while (j < m &&
+                   haystack.charAt(i + j) == needle.charAt(j)) {
+
+                j++;
+            }
+
+            if (j == m) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
