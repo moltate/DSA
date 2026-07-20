25473e4c2a9446bab203e4b1df84813b94094c95
Ankit Pandey <ankitdp7275@gmail.com>
2026-07-20 23:44:16 +0530

    Update Day30.java

--- Day30.java
+++ Day30.java
@@ -94,3 +94,32 @@ class Solution {
         }
     }
 }
+
+//Check if a Parentheses String Can Be Valid
+
+class Solution {
+    public boolean canBeValid(String s, String locked) {
+        int n = s.length();
+        if(n % 2 != 0) return false;
+        int balance = 0;
+
+        for (int i = 0; i < n ; i++){
+            if(locked.charAt(i) == '0' || s.charAt(i) == '('){
+                balance++;
+            }else{
+                balance--;
+            }
+            if(balance<0) return false;
+        }
+        balance = 0;
+        for(int i = n -1 ; i >= 0 ; i--){
+            if(locked.charAt(i) == '0' || s.charAt(i) == ')'){
+                balance++;
+            }else{
+                balance--;
+            }
+            if(balance < 0) return false;
+        }
+        return true;
+    }
+}
