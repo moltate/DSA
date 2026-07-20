cb4ac8e4fde913cd8bd141da8c798bfc9064d967
Ankit Pandey <ankitdp7275@gmail.com>
2026-07-20 23:02:21 +0530

    Update Day30.java

--- Day30.java
+++ Day30.java
@@ -72,3 +72,25 @@ class Solution {
         return -1;
     }
 }
+
+//Generate Parentheses
+
+class Solution {
+    List<String> ans = new ArrayList<>();
+    public List<String> generateParenthesis(int n) {
+        backtrack("",0,0,n);
+        return ans;
+    }
+    public void backtrack(String curr, int open, int close, int n){
+        if(curr.length() == 2*n){
+            ans.add(curr);
+            return;
+        }
+        if(open < n){
+            backtrack(curr + "(", open + 1, close, n);
+        }
+        if(close < open){
+            backtrack(curr + ")" , open, close + 1, n);
+        }
+    }
+}
