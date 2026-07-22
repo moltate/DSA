4231f006302d9efffb42b7f79366b2a326b1f9a3
Ankit Pandey <ankitdp7275@gmail.com>
2026-07-22 22:13:38 +0530

    Update Day31.java

--- Day31.java
+++ Day31.java
@@ -43,3 +43,26 @@ class Solution {
         return write;
     }
 }
+
+//Letter Combinations of a Phone Number
+
+class Solution {
+     List<String> ans = new ArrayList<>();
+
+    String[] map = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    public List<String> letterCombinations(String digits) {
+        if(digits.length()==0)   return ans;
+        backtrack(digits,0,"");
+        return ans;
+    }
+    public void backtrack(String digits, int index, String curr){
+        if(index == digits.length()){
+            ans.add(curr);
+            return;
+        }
+        String letters = map[digits.charAt(index) - '0'];
+        for(int i = 0; i < letters.length(); i++){
+            backtrack(digits, index + 1, curr + letters.charAt(i));
+        }
+    }
+}
