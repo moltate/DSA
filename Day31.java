7436b7ee215f805f7d3797445bd8a19a2e6c63ee
Ankit Pandey <ankitdp7275@gmail.com>
2026-07-22 23:28:12 +0530

    Update Day31.java

--- Day31.java
+++ Day31.java
@@ -66,3 +66,24 @@ class Solution {
         }
     }
 }
+
+//Subsets
+
+class Solution {
+     List<List<Integer>> ans = new ArrayList<>();
+    public List<List<Integer>>  subsets(int[] nums) {
+        backtrack(nums, 0, new ArrayList<>());
+        return ans;
+    }
+    
+    public void backtrack(int[] nums, int index, List<Integer>curr){
+        if(index == nums.length){
+            ans.add(new ArrayList<>(curr));
+            return;
+        }
+        curr.add(nums[index]);
+        backtrack(nums, index +1, curr);
+        curr.remove(curr.size()-1);
+        backtrack(nums, index +1, curr);
+    }
+}
