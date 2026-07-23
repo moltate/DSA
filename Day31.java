4f587d5231a07572b3458189f10cb1a14ea6c233
Ankit Pandey <ankitdp7275@gmail.com>
2026-07-24 01:04:12 +0530

    Update Day31.java

--- Day31.java
+++ Day31.java
@@ -131,3 +131,20 @@ class Solution {
         return ans.toString();
     }
 }
+
+//Plus One
+
+class Solution {
+    public int[] plusOne(int[] digits) {
+        for(int i = digits.length -1 ; i >=0; i--){
+            if(digits[i] < 9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] ans = new int [digits.length + 1];
+        ans[0]= 1;
+        return ans;
+    }
+}
