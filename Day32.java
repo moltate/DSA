4ccf98709164d6c0cf32a9975aeb6f6e38a55699
Ankit Pandey <ankitdp7275@gmail.com>
2026-07-25 22:02:23 +0530

    Update Day32.java

--- Day32.java
+++ Day32.java
@@ -15,3 +15,34 @@ class Solution {
         return length;
     }
 }
+
+//Happy Number
+
+class Solution {
+
+    public boolean isHappy(int n) {
+
+        int slow = n;
+        int fast = n;
+
+        do {
+            slow = squareSum(slow);
+            fast = squareSum(squareSum(fast));
+        } while (slow != fast);
+
+        return slow == 1;
+    }
+
+    public int squareSum(int n) {
+
+        int sum = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+
+        return sum;
+    }
+}
