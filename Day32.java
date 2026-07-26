13f024a5f6382e2e85a5acc99fa2e9dc775d76b2
Ankit Pandey <ankitdp7275@gmail.com>
2026-07-26 21:51:06 +0530

    Update Day32.java

--- Day32.java
+++ Day32.java
@@ -46,3 +46,20 @@ class Solution {
         return sum;
     }
 }
+
+//Maximum Product of the Numbers
+
+
+class Solution {
+    public int maximumProduct(int[] nums) {
+
+        Arrays.sort(nums);
+
+        int n = nums.length;
+
+        return Math.max(
+            nums[0] * nums[1] * nums[n - 1],
+            nums[n - 1] * nums[n - 2] * nums[n - 3]
+        );
+    }
+}
