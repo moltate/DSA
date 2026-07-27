428714933858a3b498f4fc764b1aa1faf9e7b364
Ankit Pandey <ankitdp7275@gmail.com>
2026-07-27 10:30:53 +0530

    Update Day32.java

--- Day32.java
+++ Day32.java
@@ -82,3 +82,24 @@ class Solution {
         return (max1 -1 ) * (max2 -1);
     }
 }
+
+//Maximum Product of Two Digits
+
+class Solution {
+    public int maxProduct(int n) {
+        int max1 = 0 ;
+        int max2 = 0;
+        
+        while(n > 0){
+            int num = n % 10;
+            if(num > max1){
+                max2 = max1;
+                max1 = num;
+            }else if(num> max2){
+                max2 = num;
+            }
+            n = n/10;
+        }
+        return max1 * max2;
+    }
+}
