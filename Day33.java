bab3efbcb2a5b296a8bd6a19628e0262bc9d4b8d
Ankit Pandey <ankitdp7275@gmail.com>
2026-07-29 12:51:35 +0530

    Update Day33.java

--- Day33.java
+++ Day33.java
@@ -33,3 +33,34 @@ class Solution {
         return new String(ans);
     }
 }
+
+//Combination Sum
+
+class Solution {
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+
+        List<List<Integer>> ans = new ArrayList<>();
+        helper(candidates, target, 0, new ArrayList<>(), ans);
+        return ans;
+    }
+
+    public void helper(int[] candidates, int target, int index,
+                       List<Integer> list, List<List<Integer>> ans) {
+
+        if (target == 0) {
+            ans.add(new ArrayList<>(list));
+            return;
+        }
+
+        if (target < 0 || index == candidates.length) {
+            return;
+        }
+        
+        list.add(candidates[index]);
+        helper(candidates, target - candidates[index], index, list, ans);
+        list.remove(list.size() - 1);
+
+        helper(candidates, target, index + 1, list, ans);
+    }
+}
