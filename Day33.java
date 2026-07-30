f720bdafffa54864ad91c634ea8e81270c2f6fa6
Ankit Pandey <ankitdp7275@gmail.com>
2026-07-31 00:55:26 +0530

    Update Day33.java

--- Day33.java
+++ Day33.java
@@ -149,3 +149,19 @@ class Solution {
         return grid[m - 1][n - 1];
     }
 }
+
+//Minimum Number of Pushes to Type Word I
+
+class Solution {
+
+    public int minimumPushes(String word) {
+
+        int ans = 0;
+
+        for (int i = 0; i < word.length(); i++) {
+            ans += (i / 8) + 1;
+        }
+
+        return ans;
+    }
+}
