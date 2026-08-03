4ec2c5dc528368b080f4cbce03a5bed5340742c3
Ankit Pandey <ankitdp7275@gmail.com>
2026-08-03 22:25:12 +0530

    Update Day35.java

--- Day35.java
+++ Day35.java
@@ -104,3 +104,27 @@ class Solution {
         return dp[0][n - 1] > 0;
     }
 }
+
+//Stone Game III
+
+class Solution {
+    public String stoneGameIII(int[] stoneValue) {
+        int n = stoneValue.length;
+        int[] dp = new int[n+1];
+        for(int i = n - 1; i >= 0; i--){
+             dp[i] = Integer.MIN_VALUE;
+             int sum = 0 ;
+             for(int k = 0; k < 3 && i + k < n ; k++){
+                sum += stoneValue[i + k];
+                dp[i] = Math.max(dp[i] , sum - dp[i + k + 1]);
+             }
+        }
+        if (dp[0] > 0)
+            return "Alice";
+
+        if (dp[0] < 0)
+            return "Bob";
+
+        return "Tie";
+    }
+}
