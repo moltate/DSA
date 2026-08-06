3292e01198e9b25ef1e19bf946fcf639731449c4
Ankit Pandey <ankitdp7275@gmail.com>
2026-08-06 21:32:36 +0530

    Update Day35.java

--- Day35.java
+++ Day35.java
@@ -157,3 +157,25 @@ class Solution {
         return ans;
     }
 }
+
+//Smallest Divisible Digit Product I
+
+class Solution {
+    public int smallestNumber(int n, int t) {
+        while(true){
+            int product = digitProduct(n);
+            if(product % t == 0){
+                return n++;
+            }
+             n++;
+        }
+    }
+    private int digitProduct(int num){
+        int product = 1;
+        while(num > 0){
+           product *= num % 10;
+           num /= 10;
+        }
+        return product;
+    }
+}
