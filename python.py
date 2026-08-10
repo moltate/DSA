6ed76a487ab4ba0f08777c63f06367eef07e2231
Ankit Pandey <ankitdp7275@gmail.com>
2026-08-10 20:02:31 +0530

    Update python.py

--- python.py
+++ python.py
@@ -12,3 +12,20 @@ class Solution:
              else:
                  r = m
          return nums[l]
+
+#Stone Game IV
+
+class Solution:
+    def winnerSquareGame(self, n: int) -> bool:
+        dp = [False] * (n + 1)
+
+        for i in range(1,n+1):
+            j = 1
+
+            while j * j <= i:
+                if not dp[i - j * j]:
+                    dp[i] = True
+                    break
+                j += 1
+        
+        return dp[n]
