4b3ae14d12af1bf603d1b5b40e7e816619016736
Ankit Pandey <ankitdp7275@gmail.com>
2026-08-24 21:24:48 +0530

    Time: 0 ms (100%), Space: 42.1 MB (40.12%) - LeetHub

--- 2481-strictly-palindromic-number/2481-strictly-palindromic-number.java
+++ 2481-strictly-palindromic-number/2481-strictly-palindromic-number.java
@@ -1,5 +1,20 @@
 class Solution {
     public boolean isStrictlyPalindromic(int n) {
-        return false;
+        boolean value = true;
+        for(int i=2;i<n-1;i++){
+            if(!check(Integer.toString(n,i))){
+               value = false;
+               break;
+            }
+        }
+        return value;
+    }
+    public boolean check(String n){
+        int left = 0, right = n.length()-1;
+        while(left<right){
+            if(n.charAt(left) != n.charAt(right)) return false;
+            left++; right--;
+        }
+        return true;
     }
 }
