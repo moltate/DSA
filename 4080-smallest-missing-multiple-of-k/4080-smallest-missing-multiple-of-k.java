bbeaf69ececadbd7c378145e35b5e9922cb49ec6
Ankit Pandey <ankitdp7275@gmail.com>
2026-08-25 21:06:14 +0530

    Time: 2 ms (68.2%), Space: 45.8 MB (6.83%) - LeetHub

---
+++ 4080-smallest-missing-multiple-of-k/4080-smallest-missing-multiple-of-k.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int missingMultiple(int[] nums, int k) {
+        Set<Integer> value = new HashSet<>();
+        for(int num : nums){
+            value.add(num);
+        }
+        int curr = k;
+        while(value.contains(curr)){
+            curr += k;
+        }
+        return curr;
+    }
+}
