c14ac4b92b41077ea193869fdabd05b5d23965a9
Ankit Pandey <ankitdp7275@gmail.com>
2026-09-01 22:09:47 +0530

    1951-FIND-THE-WINNER-OF-THE-CIRCULAR-GAME Stats: Time: 0 ms (100%), Space: 41.7 MB (98.43%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
@@ -1,9 +1,9 @@
 class Solution {
     public int findTheWinner(int n, int k) {
-        return winner(n,k)+1;
+    int ans = 0 ;
+    for(int i = 2; i <=n; i++){
+        ans = (ans+k)%i;
     }
-    private int winner(int n, int k){
-        if(n==1) return 0;
-        return (winner(n-1,k)+k)%n;
+    return ans + 1;
     }
 }
