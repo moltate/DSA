21dfa44307a20440e7f386b89ea7b3527172e3a1
Ankit Pandey <ankitdp7275@gmail.com>
2026-09-07 20:44:10 +0530

    0977-DISTINCT-SUBSEQUENCES-II Stats: Time: 5 ms (54.61%), Space: 43.2 MB (70.21%) - LeetHub

--- 0977-distinct-subsequences-ii/0977-distinct-subsequences-ii.java
+++ 0977-distinct-subsequences-ii/0977-distinct-subsequences-ii.java
@@ -1,6 +1,6 @@
 class Solution {
     public int distinctSubseqII(String s) {
-        int MOD = 1000000007;
+        int MOD = 1_00_00_00_007;
         int total = 0;
         int[] dp = new int[26];
         for(int i = 0; i < s.length(); i++){
