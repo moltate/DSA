99d723de26b7f7431af2016c345d6736ed4d5426
Ankit Pandey <ankitdp7275@gmail.com>
2026-09-08 20:15:48 +0530

    4245-COUNT-COMMAS-IN-RANGE Stats: Time: 0 ms (100%), Space: 42.5 MB (72.85%) - LeetHub

--- 4245-count-commas-in-range/4245-count-commas-in-range.java
+++ 4245-count-commas-in-range/4245-count-commas-in-range.java
@@ -1,11 +1,8 @@
 class Solution {
     public int countCommas(int n) {
-      int ans = 0;
-      for(int i = 0; i <= n; i++){
-          if(i > 999){
-            ans += 1;
-          }
-      }  
-      return ans;
+        if(n < 1000) {
+            return 0;
+        }
+        return (n-1000)+1;
     }
 }
