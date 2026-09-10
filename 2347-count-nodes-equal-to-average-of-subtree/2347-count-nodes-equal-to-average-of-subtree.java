ebc7fd1b476d96d005020ac148da33bb91e08cdb
Ankit Pandey <ankitdp7275@gmail.com>
2026-09-10 23:05:01 +0530

    2347-COUNT-NODES-EQUAL-TO-AVERAGE-OF-SUBTREE Stats: Time: 0 ms (100%), Space: 45.4 MB (63.38%) - LeetHub

--- 2347-count-nodes-equal-to-average-of-subtree/2347-count-nodes-equal-to-average-of-subtree.java
+++ 2347-count-nodes-equal-to-average-of-subtree/2347-count-nodes-equal-to-average-of-subtree.java
@@ -1,16 +1,18 @@
 class Solution {
+    int ans = 0;
+
     public int averageOfSubtree(TreeNode root) {
-        dfs(root);
+        trav(root);
         return ans;
     }
-        int ans = 0;
-    public int[] dfs(TreeNode node){
-        if(node == null) return new int[]{0,0};
-        int[] left = dfs(node.left);
-        int[] right = dfs(node.right);
-        int sum = left[0] + right[0] + node.val;
+
+    private int[] trav(TreeNode root) {
+        if (root == null) return new int[]{0, 0};
+        int[] left = trav(root.left);
+        int[] right = trav(root.right);
+        int sum = left[0] + right[0] + root.val;
         int count = left[1] + right[1] + 1;
-        if(sum / count == node.val) ans++;
-        return new int[]{sum, count};
+        if (sum / count == root.val) ans++;
+        return new int[]{sum,count};
     }
 }
