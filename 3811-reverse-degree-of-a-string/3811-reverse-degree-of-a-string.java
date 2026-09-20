b1345698382077a044fe943a6ea211b7cad61bb1
Ankit Pandey <ankitdp7275@gmail.com>
2026-09-21 01:53:23 +0530

    3811-REVERSE-DEGREE-OF-A-STRING Stats: Time: 1 ms (100%), Space: 43.9 MB (67.14%) - LeetHub

--- 3811-reverse-degree-of-a-string/3811-reverse-degree-of-a-string.java
+++ 3811-reverse-degree-of-a-string/3811-reverse-degree-of-a-string.java
@@ -1,12 +1,8 @@
 class Solution {
     public int reverseDegree(String s) {
         int sum = 0;
-        for(int i = 0; i<s.length(); i++){
-            char c =s.charAt(i);
-            int reverseVal = 26 -(c - 'a');
-            int pos = i+1;
-            sum += reverseVal * pos;
-        }
+        for(int i = 0; i<s.length(); i++)
+            sum += (26 - (s.charAt(i) - 'a')) *(i+1);
         return sum;
     }
 }
